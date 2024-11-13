13921af1f94f2e8e42cbcaea500a3ce4a4578587
LuFass <lfas@d-velop.de>
2024-11-14 00:11:30 +0100

    Login-Page

--- gui/Software-Engineering/src/app/app.routes.ts
+++ gui/Software-Engineering/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Login',
+        title: 'Login Menü',
+        component: LoginComponent,
+    },
+    {
+        path: 'Register',
+        title: 'Register Menü',
+        component: RegisterComponent,
+    },
+];
