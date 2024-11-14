048a0a50aa8e48cfa71793d4fd5dcaaaaf084f4f
Jackson085 <jackson26594916@gmail.com>
2024-11-14 16:50:50 +0100

    Update StartAll.py

--- UnitTests/StartAll.py
+++ UnitTests/StartAll.py
@@ -6,8 +6,8 @@ from UnitTests.Validation.UserServiceTest import UserServiceTest
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
 
-    test_suite.addTest(unittest.makeSuite(CategoriesServiceTest))
-    test_suite.addTest(unittest.makeSuite(UserServiceTest))
-    test_suite.addTest(unittest.makeSuite(LoginControllerTest))
+    test_suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(CategoriesServiceTest))
+    test_suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(UserServiceTest))
+    test_suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(LoginControllerTest))
 
     unittest.TextTestRunner().run(test_suite)
