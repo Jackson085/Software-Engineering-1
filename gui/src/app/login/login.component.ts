efeb30518260cf896eb5fbdf0912ff93099218c2
Hintemann, Niklas <niklas.hintemann@d-velop.de>
2024-11-27 20:06:14 +0100

    added error msseges

--- gui/src/app/login/login.component.ts
+++ gui/src/app/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email = signal<string>('');
+  password = signal<string>('');
+  isEmailBlur = signal<boolean>(false)
+  hasLoginAttempted = signal<boolean>(false);
+  isLoginValid = signal<boolean>(false)
+  
+  isEmailValid = computed(() => EMAIL_REGEX.test(this.email()))
 
+  onEmailInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.email.set(input.value);
+  }
+
+  onEmailBlur(): void {
+    this.isEmailBlur.set(true);
+  }
+
+  onPasswordInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.email.set(input.value);
+  }
 }
+
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
